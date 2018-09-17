8ba6c4e8fb69dca7bdae1197b31c1627edf857bd
HelmerFuentes <elmer.fuentes@hotmail.com>
2018-09-17 15:31:38 -0500

    nhnh

--- Trabajo/presentacion/Form1.cs
+++ Trabajo/presentacion/Form1.cs
@@ -7,15 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Entidades;
-using Logica;
 
 namespace presentacion
 {
     public partial class Form1 : Form
     {
-        Estudiantes estudiante = new Estudiantes();
-        Logica.LogicaEstudiantes logEst = new Logica.LogicaEstudiantes();
         public Form1()
         {
             InitializeComponent();
@@ -23,20 +19,7 @@ namespace presentacion
 
         private void btnMatricular_Click(object sender, EventArgs e)
         {
-            int numero;
-            String valido = "";
-            if (txtIdentificacion.Text.Length >= 8 && txtIdentificacion.Text.Length <= 12 && int.TryParse(txtIdentificacion.Text, out numero))
-            {
-                valido = logEst.validarEstudiante(txtIdentificacion.Text);
-                if (!valido.Equals("Existe"))
-                {
-                    MessageBox.Show("El Estudiante no existe.");
-                }
-            }
-            else
-            {
-                
-            }
+
         }
     }
 }
