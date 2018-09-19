7c602160a59fa4c9f9eb64e422e6bf7c8c5fc0e6
RmrGnzlzLpz <rmrgnzlzlpz@gmail.com>
2018-09-19 15:22:28 -0500

    80% fase presentación.

---
+++ Trabajo/logica/LogicaGrupo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica
+{
+    public class LogicaGrupo
+    {
+
+        public string disponibilidad(int codigoGrupo)
+        {
+            return "Grupo disponible || Grupo no disponible || Grupo no existe.";
+        }
+    }
+}
