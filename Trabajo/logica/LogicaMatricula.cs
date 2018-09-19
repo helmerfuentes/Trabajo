66ed66f1a426ac3c8e179d54fe6857694dfdfafb
Ivan Contreras <ivancontry.13@gmail.com>
2018-09-19 14:48:33 -0500

    es ese

--- Trabajo/logica/LogicaMatricula.cs
+++ Trabajo/logica/LogicaMatricula.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Datos;
 
 namespace logica
 {
     class LogicaMatricula
     {
+
+        DatosMatricula matricula = new DatosMatricula();
+
+        public String asignaturaSeleccionada(String asignatura, string identificacion)
+        {
+            DataTable table = new DataTable();
+            table = matricula.buscarAsignatura(asignatura,identificacion);
+            if (table.Rows.Count > 0)
+            {
+                return "Existe";
+            }
+            else
+            {
+                return "No existe";
+
+            }
+
+        }
     }
 }
